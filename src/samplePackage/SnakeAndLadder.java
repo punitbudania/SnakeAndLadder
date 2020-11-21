2235a31535082100b3db01437e48d583b39d4e25
punitbudania <punitbudania88@gmail.com>
2020-11-21 19:57:01 +0530

    [punit]: 2 players game UC7

--- src/samplePackage/SnakeAndLadder.java
+++ src/samplePackage/SnakeAndLadder.java
@@ -6,39 +6,80 @@ public class SnakeAndLadder
 	{
 		final int ladder = 1;
 		final int snake = 2;
-		int position = 0;
-		int count = 0;
+		int playerA = 0;
+		int playerB = 0;
+		int unit = 1;
+		String player = "no one";
+		boolean found = false;
 		
-		while (position<100)
+		while(!found)
 		{
-			count++;
 			int dice = (int) Math.floor(Math.random()*100%6);
 			int die = dice + 1;
-			
-			int check = (int) Math.floor(Math.random()*10)%3;
-			switch (check)
+		
+			if (unit==1)
 			{
-				case ladder:
-					position += die;
-					if (position>100)
-					{
-						position -= die;
-					}
-					break;
-				case snake:
-					position -= die;
-					if (position<0)
-					{
-						position = 0;
-					}
-					break;
-				default:
-					position += 0;
+				unit=2;
+				int check = (int) Math.floor(Math.random()*10)%3;
+				switch (check)
+				{
+					case ladder:
+						playerA += die;
+						if (playerA>100)
+						{
+							playerA -= die;
+						}
+						unit=1;
+						break;
+					case snake:
+						playerA -= die;
+						if (playerA<0)
+						{
+							playerA = 0;
+						}
+						break;
+					default:
+						playerA += 0;
+				}
+				player = "playerA";
+				if (playerA==100)
+				{
+					found = true;
+				}
 			}
 			
-			System.out.println("Position: " + position);
+			else
+			{
+				unit=1;
+				int check = (int) Math.floor(Math.random()*10)%3;
+				switch (check)
+				{
+					case ladder:
+						playerB += die;
+						if (playerB>100)
+						{
+							playerB -= die;
+						}
+						unit=2;
+						break;
+					case snake:
+						playerB -= die;
+						if (playerB<0)
+						{
+							playerB = 0;
+						}
+						break;
+					default:
+						playerB += 0;
+				}
+				player = "playerB";
+				if (playerB==100)
+				{
+					found = true;
+				}
+			}
 		}
 		
-		System.out.println("Count: " + count);
+		System.out.println(player + " won the game");
 	}
 }
