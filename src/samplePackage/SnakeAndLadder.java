1ce9be43dbb9d417125837ee1f99546781145d0e
punitbudania <punitbudania88@gmail.com>
2020-11-21 14:40:58 +0530

    [punit]: Players First Move UC3

--- src/samplePackage/SnakeAndLadder.java
+++ src/samplePackage/SnakeAndLadder.java
@@ -7,7 +7,23 @@ public class SnakeAndLadder
 		int position = 0;
 		int dice = (int) Math.floor(Math.random()*100%6);
 		int die = dice + 1;
-		position += die;
+		
+		final int ladder = 1;
+		final int snake = 2;
+		int check = (int) Math.floor(Math.random()*10)%3;
+		System.out.println(check);
+		switch (check)
+		{
+			case ladder:
+				position += die;
+				break;
+			case snake:
+				position -= die;
+				break;
+			default:
+				position += 0;
+		}
+		
 		System.out.println("Position: " + position);
 	}
 }
