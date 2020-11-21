74ac920dea407a7c3314924fcabcac83daf3fcaf
punitbudania <punitbudania88@gmail.com>
2020-11-21 14:13:52 +0530

    [punit]: DieRollUC2 added

--- src/samplePackage/SnakeAndLadder.java
+++ src/samplePackage/SnakeAndLadder.java
@@ -5,6 +5,9 @@ public class SnakeAndLadder
 	public static void main(String[] args)
 	{
 		int position = 0;
+		int dice = (int) Math.floor(Math.random()*100%6);
+		int die = dice + 1;
+		position += die;
 		System.out.println("Position: " + position);
 	}
 }
