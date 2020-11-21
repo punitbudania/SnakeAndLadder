90643ebbebd5dc9cc3c9fa3ad98b322e0944b9b8
punitbudania <punitbudania88@gmail.com>
2020-11-21 14:59:52 +0530

    [punit]: Player Won UC4

--- src/samplePackage/SnakeAndLadder.java
+++ src/samplePackage/SnakeAndLadder.java
@@ -4,26 +4,32 @@ public class SnakeAndLadder
 {
 	public static void main(String[] args)
 	{
-		int position = 0;
-		int dice = (int) Math.floor(Math.random()*100%6);
-		int die = dice + 1;
-		
 		final int ladder = 1;
 		final int snake = 2;
-		int check = (int) Math.floor(Math.random()*10)%3;
-		System.out.println(check);
-		switch (check)
-		{
-			case ladder:
-				position += die;
-				break;
-			case snake:
-				position -= die;
-				break;
-			default:
-				position += 0;
-		}
+		int position = 0;
 		
+		while (position<100)
+		{
+			int dice = (int) Math.floor(Math.random()*100%6);
+			int die = dice + 1;
+			
+			int check = (int) Math.floor(Math.random()*10)%3;
+			switch (check)
+			{
+				case ladder:
+					position += die;
+					break;
+				case snake:
+					position -= die;
+					if (position<0)
+					{
+						position = 0;
+					}
+					break;
+				default:
+					position += 0;
+			}
+		}
 		System.out.println("Position: " + position);
 	}
 }
